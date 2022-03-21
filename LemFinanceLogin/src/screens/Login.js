dfbf933558c5e8477896f5117e6113c55e3cec8a
mathias-pl <66693929+mathias-pl@users.noreply.github.com>
2022-03-21 17:02:38 -0400

    Login-ui
    Création sur BuilderX d'une interface utilisateur pour l'accueil, la connexion et la création de compte. Le code React Native devra être peaufiné et des fonctions devront être ajoutés pour être connecté au serveur et à l'API

---
+++ LemFinanceLogin/src/screens/Login.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Image } from "react-native";
+import MaterialButtonPrimary from "../components/MaterialButtonPrimary";
+import MaterialUnderlineTextbox from "../components/MaterialUnderlineTextbox";
+
+function Login(props) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/logo-transparent_bllL..png")}
+        resizeMode="contain"
+        style={styles.logo}
+      ></Image>
+      <MaterialButtonPrimary
+        caption="CONNEXION"
+        style={styles.connexionButton}
+      ></MaterialButtonPrimary>
+      <MaterialUnderlineTextbox
+        inputStyle="Placeholder"
+        inputStyle="   EMAIL"
+        style={styles.emailInput}
+      ></MaterialUnderlineTextbox>
+      <MaterialUnderlineTextbox
+        inputStyle="Placeholder"
+        inputStyle="   MOT DE PASSE"
+        style={styles.passwordInput}
+      ></MaterialUnderlineTextbox>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    height: 200,
+    width: 200,
+    marginTop: 150,
+    alignSelf: "center"
+  },
+  connexionButton: {
+    height: 46,
+    width: 274,
+    borderRadius: 12,
+    backgroundColor: "rgba(77,166,169,1)",
+    marginTop: 331,
+    marginLeft: 51
+  },
+  emailInput: {
+    height: 43,
+    width: 300,
+    borderRadius: 12,
+    backgroundColor: "rgba(230, 230, 230,1)",
+    marginTop: -192,
+    marginLeft: 38
+  },
+  passwordInput: {
+    height: 43,
+    width: 300,
+    borderRadius: 12,
+    backgroundColor: "rgba(230, 230, 230,1)",
+    marginTop: 18,
+    marginLeft: 38
+  }
+});
+
+export default Login;
