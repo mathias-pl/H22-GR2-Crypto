94ff48ec623394a7fe4c1c21357f4599f44adb52
Luxyray <LuxyRay111@gmail.com>
2022-04-04 13:47:07 -0400

    Deleted ancienne page

--- LemFinance/App.js
+++ LemFinance/App.js
@@ -5,13 +5,18 @@ import PageZeroScreen from "./src/screens/SignInScreen/PageZeroScreen";
 import Home from "./src/screens/SignInScreen/Home";
 import Login from "./src/screens/SignInScreen/Login";
 import Signup from "./src/screens/SignInScreen/Signup";
+import Accueil from "./src/screens/SignInScreen/Accueil";
+import LogInScreen from "./src/screens/SignInScreen/LogInScreen";
 
 export default function App() {
   return (
     <View style={styles.root}>
       {/*<Home/>*/}
       {/*<Login/>*/}
-      <Signup/>
+      {/*<Signup/>*/}
+      {/*<Accueil/>*/}
+
+      <PageZeroScreen></PageZeroScreen>
     </View>
   );
 }
